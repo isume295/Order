8220d425c5aa055f5ca2eca06da7bd19f47ed741
Sumeya <sumeyaibrahim72@gmail.com>
2023-11-14 20:17:44 +0300

    Add Header

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+ 
+?>
+
+<html lang="en">
+<?php include('templates/header.php'); ?>
+<body>
+    <h1>users profile page</h1>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Pizza</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand{
+            background: #cbb09c !important;
+        }
+        .brand-text{
+            color: #cbb09c !important;
+        }
+    </style>
+
+</head>
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">My Pizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-down">
+                <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+                <!-- <li><a href="#"></a></li>
+                <li><a href="#"></a></li> -->
+            </ul>
+        </div>
+    </nav>
